e19ee83ed9c6c3a31c78b15145d2b16c170e02f6
Emanuel Serveleon <123126936+emanuel-serveleon@users.noreply.github.com>
2023-11-15 12:57:22 -0500

    Add background color change when hovering in a div

--- javascript.js
+++ javascript.js
@@ -1,8 +1,16 @@
-const container = document.querySelector(".container")
+const container = document.querySelector(".container");
 
 for (let i = 0; i < 16*16; i++) {
   const div = document.createElement("div");
-  div.classList = "square"
+  div.classList = "square";
 
-  container.appendChild(div)
+  container.appendChild(div);
 }
+
+const squares = document.querySelectorAll(".square");
+
+squares.forEach((square) => {
+  square.addEventListener("mouseenter", () => {
+    square.style.backgroundColor = "blue";
+  })
+})
