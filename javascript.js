f9b9a3bb3d808ee5a53d23eb325536976f806884
Emanuel Serveleon <123126936+emanuel-serveleon@users.noreply.github.com>
2023-11-15 14:03:56 -0500

    Add button to create new grid with different size

--- javascript.js
+++ javascript.js
@@ -1,16 +1,33 @@
 const container = document.querySelector(".container");
+const createGridButton = document.querySelector("button");
 
-for (let i = 0; i < 16*16; i++) {
-  const div = document.createElement("div");
-  div.classList = "square";
+createGridButton.addEventListener("click", () => {
+  let squarePerSide;
+  do {
+    squarePerSide = prompt("Enter the number of square per side (1-100):")
+  } while (squarePerSide < 1 || squarePerSide > 100);
+  createGrid(squarePerSide);
+})
 
-  container.appendChild(div);
+function createGrid(size) {
+  container.textContent = ""
+
+  const width = `${container.offsetWidth / size}px`;
+
+  for (let i = 0; i < size * size; i++) {
+    const div = document.createElement("div");
+    div.classList = "square";
+    div.style.width = width;
+    div.style.height = width;
+
+    container.appendChild(div);
+  }
+
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.addEventListener("mouseenter", () => {
+      square.style.backgroundColor = "blue";
+    })
+  })
 }
 
-const squares = document.querySelectorAll(".square");
-
-squares.forEach((square) => {
-  square.addEventListener("mouseenter", () => {
-    square.style.backgroundColor = "blue";
-  })
-})
